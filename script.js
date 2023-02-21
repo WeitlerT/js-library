99db8639b38d6c82f26d695a7924f5c339998249
Tyler Weitler <tweitler@gmail.com>
2023-02-21 17:31:28 -0500

    Added basic logic
    Have array items displaying through a function that populates HTML. Have a very basic version of the form adding a new book working but need to check for duplication.

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 let myLibrary = [];
 const libraryDiv = document.getElementById("library");
+const addBook = document.getElementById("addBook");
+const closeModal = document.getElementById("closeModal");
+const modalContainer = document.getElementById("modalContainer");
+const delBtn = document.getElementById("delBtn");
+const toggleBtn = document.getElementById("toggleBtn");
 
+// Listeners
+addBook.addEventListener('click', () => {
+    modalContainer.classList.add('show');
+});
 
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+});
+
+// Get form data
+function getData(form) {
+    var formData = new FormData(form);
+    let entryArr = [];
+
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+      entryArr.push(pair[1]);
+    }
+
+    console.log(Object.fromEntries(formData));
+
+    console.log(`This is the new entry ${entryArr}`);
+    let newEntry = new Book(entryArr[0],entryArr[1],entryArr[2],entryArr[3]);
+    console.log(newEntry);
+    myLibrary.push(newEntry.getInfo());
+    console.log(myLibrary);
+    populateLibrary();
+  }
+  
+  document.getElementById("myForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    getData(e.target);
+  });
+  
+
+//Book constructor
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -17,17 +57,35 @@ Book.prototype.getInfo = function(){
 
 const harryPotter = new Book('Harry Potter Phil Stone', 'JK Rowling', 255, false);
 
-console.log(harryPotter.getInfo());
+//Adding some books to start
 myLibrary.push(harryPotter.getInfo());
+myLibrary.push(["Cool book", "cool author", 100, false])
+myLibrary.push(["Hunger Games", "Stephen King", 125, false])
+myLibrary.push(["Green eggs n ham", "Dr.Seuss", 150, false])
+
 console.log(myLibrary);
+console.log(typeof(myLibrary[0]));
+console.log(typeof(myLibrary[1]));
 
-
+// Populate Library Function to add books to cards
 function populateLibrary(){
     for (let i=0; i<myLibrary.length; i++ ){
-        let newEntry = document.createElement('p');
+        let newEntry = document.createElement('div');
         newEntry.className = "card";
+        newEntry.dataset.num = i;
         newEntry.innerText = myLibrary[i];
-        libraryDiv.appendChild(newEntry)
+
+        let delButton = document.createElement('button');
+        delButton.setAttribute("id","delBtn");
+        delButton.innerText = "Remove";
+
+        let toggleButton = document.createElement('button');
+        toggleButton.setAttribute("id","toggleBtn");
+        toggleButton.innerHTML = "Toggle Read";
+
+        libraryDiv.appendChild(newEntry);
+        newEntry.appendChild(delButton);
+        newEntry.appendChild(toggleButton);
     }
 }
 
